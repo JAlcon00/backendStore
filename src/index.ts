2506354452b32e7145cc3d03b1ad45b0e586f048
JAlcon00 <c.jesusalmanza@gmail.com>
2025-06-05 15:59:17 -0600

    cambio para que se pueda consumir la API desde cualquier entorno

--- src/index.ts
+++ src/index.ts
@@ -36,8 +36,7 @@ app.use(expressWinston.logger({
 }));
 
 // Middleware
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
-app.use(cors({ origin: FRONTEND_URL }));
+app.use(cors());
 app.use(express.json());
 app.use('/uploads/articulos', express.static(path.join(__dirname, '../uploads/articulos')));
 
